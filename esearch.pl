803d625226914b8c359e5c0fa7f146e69b7d87f4
EconJoe <staudt.joe@gmail.com>
2016-10-12 17:16:55 -0400

    Add files via upload

--- esearch.pl
+++ esearch.pl
@@ -1,29 +1,55 @@
 
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
 use LWP::Simple;
-$query = 'chimpanzee[orgn]+AND+biomol+mrna[prop]';
+
+my $infile="D:\\Research\\Projects\\StemCells\\StemCells2\\Data\\stemcellcoremeshterms.txt";
+open (INFILE, "<$infile") or die "Can't open subjects file: $infile";
+
+my $outfile="D:\\Research\\Projects\\StemCells\\StemCells2\\Data\\test.txt";
+open (OUTFILE, ">$outfile") or die "Can't open subjects file: $outfile";
+
+my $query="";
+while (<INFILE>) {
+      my $stub=$_;
+      chomp($stub); 
+      if ($query eq "") { $query="$stub"; }
+      else { $query="$query+OR+$stub"; }
+}
 
 #assemble the esearch URL
-$base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
-$url = $base . "esearch.fcgi?db=nucleotide&term=$query&usehistory=y";
+my $base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
+my $utility = 'esearch';
+my $db = 'pubmed';
+my $url = "$base$utility.fcgi?db=$db&term=$query&usehistory=y";
+#print OUTFILE $url;
 
 #post the esearch URL
-$output = get($url);
+my $output = get($url);
+
+#print OUTFILE $output;
 
 #parse WebEnv, QueryKey and Count (# records retrieved)
-$web = $1 if ($output =~ /<WebEnv>(\S+)<\/WebEnv>/);
-$key = $1 if ($output =~ /<QueryKey>(\d+)<\/QueryKey>/);
-$count = $1 if ($output =~ /<Count>(\d+)<\/Count>/);
+my $web = $1 if ($output =~ /<WebEnv>(\S+)<\/WebEnv>/);
+my $key = $1 if ($output =~ /<QueryKey>(\d+)<\/QueryKey>/);
+my $count = $1 if ($output =~ /<Count>(\d+)<\/Count>/);
 
-#open output file for writing
-open(OUT, ">chimp.fna") || die "Can't open file!\n";
-
-#retrieve data in batches of 500
-$retmax = 500;
-for ($retstart = 0; $retstart < $count; $retstart += $retmax) {
-        $efetch_url = $base ."efetch.fcgi?db=nucleotide&WebEnv=$web";
+#retrieve data in batches of 100,000
+my $retmax = 100000;
+for (my $retstart = 0; $retstart < $count; $retstart += $retmax) {
+        my $efetch_url = $base ."efetch.fcgi?db=pubmed&WebEnv=$web";
         $efetch_url .= "&query_key=$key&retstart=$retstart";
-        $efetch_url .= "&retmax=$retmax&rettype=fasta&retmode=text";
-        $efetch_out = get($efetch_url);
-        print OUT "$efetch_out";
+        $efetch_url .= "&retmax=$retmax&retmode=pmid";
+        my $efetch_out = get($efetch_url);
+        print OUTFILE "$efetch_out";
 }
-close OUT;
+close OUTFILE;
+
+
+
+
+
+
+
